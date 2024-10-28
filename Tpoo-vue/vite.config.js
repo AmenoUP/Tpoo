7de12fd7c982d87d9ad1be466ea60afa95f7d9ac
Ugo Papaïx <144673541+AmenoUP@users.noreply.github.com>
2024-10-28 14:03:24 +0100

    Update vite.config.js

--- Tpoo-vue/vite.config.js
+++ Tpoo-vue/vite.config.js
@@ -16,5 +16,9 @@ export default defineConfig({
   build: {
       outDir: './src-cordova/www/index.html', // Répertoire où Vite générera le build
       emptyOutDir: true // Nettoie le répertoire de destination avant le build
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 5173, // assurez-vous que le port correspond à ce que Render attend
     }
 })
